d4c998c8559a55bee002bde6eabde53acfced56b
Frid Zina <friday.zina@gmail.com>
2025-05-01 13:28:02 +0300

    Add files

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='fakereviewdetector',
-    version='0.1.0',
-    description='Fake review classification using transformers, BiLSTM and pruning.',
-    author='Your Name',
-    author_email='your.email@example.com',
+    name="fakereviewdetector",
+    version="0.1.0",
     packages=find_packages(),
-    include_package_data=True,
     install_requires=requirements,
-    python_requires='>=3.7',
+    author="Zina",
+    description="Библиотека для классификации фиктивных отзывов с использованием трансформеров, BiLSTM и pruning.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zina-frid/fake_review_detection.git",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3.7',
+    include_package_data=True,
 )
